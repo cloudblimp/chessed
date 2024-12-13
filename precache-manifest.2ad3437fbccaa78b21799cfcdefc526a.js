4134c5e1883b3cff418048ebe8e4a743485c0b49
Projects <projects@cloudblimp.local>
2024-12-13 15:16:57 +0530

    Updates

---
+++ precache-manifest.2ad3437fbccaa78b21799cfcdefc526a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa328b8dd464dad40caa1a4698d5df5",
+    "url": "/chessed/index.html"
+  },
+  {
+    "revision": "86661cd0aa5c0a97070b",
+    "url": "/chessed/static/css/main.8af7b9c1.chunk.css"
+  },
+  {
+    "revision": "2affbc4dade5805b8aed",
+    "url": "/chessed/static/js/2.e43a638e.chunk.js"
+  },
+  {
+    "revision": "86661cd0aa5c0a97070b",
+    "url": "/chessed/static/js/main.1d81fb87.chunk.js"
+  },
+  {
+    "revision": "bd59ad88cfc2fa6adc1d",
+    "url": "/chessed/static/js/runtime~main.8a268596.js"
+  }
+]);
